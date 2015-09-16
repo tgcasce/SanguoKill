b483741e23f3f577247d0dad89a29f4140dc3cdc
Maulyn Tang <tgcasce65@163.com>
2015-09-16 20:00:20 +0800

    [add] TGCGameCard.swift file

---
+++ SanguoKill/Model/TGCGameCard.swift
@@ -0,0 +1,13 @@
+//
+//  TGCGameCard.swift
+//  SanguoKill
+//
+//  Created by DonMaulyn on 15/9/16.
+//  Copyright (c) 2015年 MaulynTang. All rights reserved.
+//
+
+import UIKit
+
+class TGCGameCard: NSObject {
+   
+}
