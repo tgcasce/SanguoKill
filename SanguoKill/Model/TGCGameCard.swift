01ff0a913a9d7f12a6b971db112596b387d3854b
Maulyn Tang <tgcasce65@163.com>
2015-09-17 11:03:54 +0800

    [modify] class TGCGameCard and enum

--- SanguoKill/Model/TGCGameCard.swift
+++ SanguoKill/Model/TGCGameCard.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
+enum TGCGameCardSuit: Character {
+    case Spades = "♠️", Hearts = "♥️", Clubs = "♣️", Diamonds = "♦️"
+}
+
+enum TGCGameCardRank: Int {
+    case Ace = 1
+    case Two, Trhee, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+}
+
 class TGCGameCard: NSObject {
-   
+    let suit: TGCGameCardSuit
+    let rank: TGCGameCardRank
+    
+    init(suit: TGCGameCardSuit, rank: TGCGameCardRank) {
+        self.suit = suit
+        self.rank = rank
+    }
 }
